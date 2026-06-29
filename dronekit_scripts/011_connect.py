0a175f73011920e5b3dfa77a538d586d55a465fc
Y Hiroka <haru.gintama@gmail.com>
2026-06-29 15:12:15 +0900

    Add 21st workshop folder with README.md

--- dronekit_scripts/011_connect.py
+++ dronekit_scripts/011_connect.py
@@ -1,14 +1,11 @@
 import time
 from dronekit import connect
 
-<<<<<<< HEAD
-#vehicle = connect('tcp:127.0.0.1:5762', wait_ready=True, timeout=60)
-vehicle = connect('tcp:10.16.119.100:5762', wait_ready=True, timeout=60)
-=======
+
 # vehicle = connect('127.0.0.1:14551', wait_ready=True, timeout=60)
 vehicle = connect('tcp:127.0.0.1:5762', wait_ready=True, timeout=60) #WSL
 
->>>>>>> 5c04340466acc5ad7c95bbb6936ffa83b46046d3
+
 
 while True:
     print ("====================================")
